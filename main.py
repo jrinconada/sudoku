4787a8a38b8a034a61035757bc394e7533e07ea9
Juan Rinconada <juan.rinconada@vanadis.es>
2023-01-05 22:51:25 +0100

    Remove usage message

--- main.py
+++ main.py
@@ -2,16 +2,6 @@ import generator
 import validator
 import output
 
-def usage():
-    return """
-    USAGE: main.py [number of sudokus] [-o out.txt]
-
-    1. If no number is given, it will generate one sudoku
-    2. If no output file is given, it will write to the console    
-
-    EXAMPLES: 
-        - 20 sudokus saved in a text file: main.py 20 -o sudokus.txt"""
-
 sudoku = generator.generate()
 output.show(sudoku)
 output.save(sudoku)
