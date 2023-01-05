4cbcea8f7bd454432f7a7b4bcc3a35c49490585e
Juan Rinconada <juan.rinconada@vanadis.es>
2023-01-05 22:44:16 +0100

    Add saving suduko in text file

--- main.py
+++ main.py
@@ -14,5 +14,6 @@ def usage():
 
 sudoku = generator.generate()
 output.show(sudoku)
+output.save(sudoku)
 valid = validator.validate(sudoku)
 print('Valid sudoku' if valid else 'Invalid sudoku')
--- output.py
+++ output.py
@@ -5,4 +5,16 @@ def show(sudoku):
         for column in row:
             print(column, end='|')
         print()
+
+def save(sudoku):
+    result = ''    
+    for row in sudoku:
+        result = result + ''.join(str(n) for n in row)
+    write('out.txt', result)
+
+def write(location, data):
+    """ Given a file path and some data, saves the data to the file.
+        Automatically creates the file if it does not exit and overwrites the content if there is something """
+    with open(location, 'w') as file:
+        file.write(data)
             
