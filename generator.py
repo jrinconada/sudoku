9336dd23f6d77043c0fe10935af7dd1231d89b72
Juan Rinconada <juan.rinconada@vanadis.es>
2023-01-09 00:54:28 +0100

    Implement a working version a recursive generator

--- generator.py
+++ generator.py
@@ -8,14 +8,15 @@ def generate(n = 9):
     return sudoku
     
 def fill(sudoku, x = 0, y = 0):        
-    row = list(range(1, 10))
-    random.shuffle(row)    
-    for n in row:
-        if y == 0 or validator.possible(sudoku, n, x, y): # No need to check first row
-            sudoku[y][x] = n            
-            fill(sudoku, x + 1 if x < len(sudoku[y]) else 0, y + 1 if x == len(sudoku[y]))
-    if len(row) == 0: # This row is filled 
-        if y < len(sudoku) - 1: # There are still empty rows
-            fill(sudoku, 0, y + 1)
-    else: # This row cannot be filled with this list of numbers, we need to try another one
-        fill(sudoku, 0, y)
+    possibilities = list(range(1, 10))
+    random.shuffle(possibilities)    
+    for number in possibilities:
+        if validator.possible(sudoku, number, x, y):
+            sudoku[y][x] = number
+            if y == len(sudoku) - 1 and x == len(sudoku[0]) - 1:
+                return True # If we arrive at the end of the sudoku, it means that all cells are filled            
+            filled = fill(sudoku, x + 1 if x < len(sudoku[y]) - 1 else 0, y + 1 if x == len(sudoku[y]) - 1 else y)
+            if filled:
+                return True # If filled is true, it means that one of the steps have filled the last cell
+            sudoku[y][x] = 0
+    return False # If we are here, there was no possible number to put in that space, so we need to go back to the previous step
