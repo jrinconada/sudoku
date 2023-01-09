392852d3f04ff4dc225c634fe923e3b7a51a8bf0
Juan Rinconada <juan.rinconada@vanadis.es>
2023-01-09 01:20:21 +0100

    Show sudoku generation stats

--- generator.py
+++ generator.py
@@ -3,11 +3,14 @@ import validator
 
 def generate(n = 9):
     """ Generate a random matrix of numbers of n by n (9 by default)"""
+    global fails
+    fails = 0
     sudoku = [[0 for x in range(n)] for y in range(n)]
-    fill(sudoku)    
+    fill(sudoku)
     return sudoku
     
 def fill(sudoku, x = 0, y = 0):        
+    global fails
     possibilities = list(range(1, 10))
     random.shuffle(possibilities)    
     for number in possibilities:
@@ -19,4 +22,5 @@ def fill(sudoku, x = 0, y = 0):
             if filled:
                 return True # If filled is true, it means that one of the steps have filled the last cell
             sudoku[y][x] = 0
+    fails = fails + 1
     return False # If we are here, there was no possible number to put in that space, so we need to go back to the previous step
--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import generator
 import validator
 import output
+import time
 
-sudoku = generator.generate()
-output.show(sudoku)
-output.save(sudoku)
-valid = validator.validate(sudoku)
-print('Valid sudoku' if valid else 'Invalid sudoku')
+total = 0
+start = time.time()
+
+for i in range(100):
+    sudoku = generator.generate()
+    output.show(sudoku)
+    output.save(sudoku)
+    valid = validator.validate(sudoku)
+    total = total + generator.fails
+    print('This is a', 'valid' if valid else 'invalid', 'sudoku.', generator.fails, 'failed tries. Average:', total / (i + 1), 'tries. Total:', total, 'tries.')
+
+print('It took', time.time() - start, 'seconds.')
+     
