73354fc0fdca7a7523fa7bb6de49f2f300b9c3fb
Juan Rinconada <jrinconada@gmail.com>
2023-01-11 12:26:59 +0100

    Display solving algorithm

--- main.py
+++ main.py
@@ -4,26 +4,35 @@ import output
 import time
 import solver
 
-NUMBER_OF_SUDOKUS = 100
-SHOW_GENERATION = False
+NUMBER_OF_SUDOKUS = 1
+SHOW_GENERATION_PROCESS = False
+SHOW_SOLVING_PROCESS = True
 SIZE = 9
-SPACES = 53 # 24 beginner, 53 expert (for a 9x9 grid)
+SPACES = 24 # 24 beginner, 53 expert (for a 9x9 grid)
 
 total = 0
 start = time.time()
 
 for i in range(NUMBER_OF_SUDOKUS):
-    sudoku = generator.generate(SIZE, SHOW_GENERATION)
-    if not SHOW_GENERATION:
+    # Generation of filled sudoku
+    sudoku = generator.generate(SIZE, SHOW_GENERATION_PROCESS)
+    if not SHOW_GENERATION_PROCESS:
         output.show(sudoku)    
     valid = validator.validate(sudoku)
     total = total + generator.fails
     print('\n\nThis is a', 'valid' if valid else 'invalid', 'sudoku.', generator.fails, 'failed tries. Average:', total / (i + 1), 'tries. Total:', total, 'tries.')
+
+    # Remove numbers
     for s in range(SPACES):    
         generator.removeNumber(sudoku)
     output.show(sudoku)
     output.save(sudoku)
-    solver.solve(sudoku)
-    output.show(sudoku)
 
-print('It took', time.time() - start, 'seconds.')
+    # Solve sudoku
+    solver.solve(sudoku, SHOW_SOLVING_PROCESS)    
+    if not SHOW_SOLVING_PROCESS:
+        output.show(sudoku)
+    print('\n\nSudoku solved. It took', solver.fails, 'tries.')
+
+# Show summary
+print(NUMBER_OF_SUDOKUS, 'sudoku generated and solved in', round(time.time() - start), 'seconds.')
--- solver.py
+++ solver.py
@@ -1,15 +1,31 @@
 import validator
+import output
 
-def solve(sudoku):
+fails = 0
+
+def solve(sudoku, display_progress = False):
+    global fails
     for y in range(len(sudoku)):
         for x in range(len(sudoku[0])):        
             if sudoku[y][x] == 0:
-                for n in range(1, len(sudoku) + 1):
-                    if validator.possible(sudoku, n, x, y):
-                        sudoku[y][x] = n
-                        if solve(sudoku):
+                for number in range(1, len(sudoku) + 1):
+                    if display_progress:
+                        output.diplay(number, x, y)
+                    if validator.possible(sudoku, number, x, y):
+                        sudoku[y][x] = number
+                        if solve(sudoku, display_progress):
                             return True
                         sudoku[y][x] = 0 # Not solved with this number
+                        fails = fails + 1
+                        if display_progress:
+                            output.delete(x, y)
+                    else:
+                        fails = fails + 1
+                        if display_progress:
+                            output.delete(x, y)
                 return False # Not solved with any number
+            else:
+                if display_progress:
+                    output.diplay(sudoku[y][x], x, y)
     return True
     
